7819c6c5e132ec2483c6ce65cf36979a77703cf4
sejutaimpian <derissulistina@gmail.com>
2023-03-02 16:43:15 +0700

    add migration: facilities

---
+++ database/migrations/2023_03_02_093914_create_facilities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('facility_name', 128);
+            $table->string('icon', 128)->nullable()->default(null);
+            $table->text('description')->nullable()->default(null);
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('facilities');
+    }
+};
