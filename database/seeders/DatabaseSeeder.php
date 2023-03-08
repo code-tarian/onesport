c5aee1b29b2ba22d06ed5c12eadfc39048a436fd
sejutaimpian <derissulistina@gmail.com>
2023-03-08 12:06:42 +0700

    feat(seeder): add FieldsSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            SpecificationSeeder::class
+            SpecificationsSeeder::class,
+            FieldsSeeder::class
         ]);
     }
 }
