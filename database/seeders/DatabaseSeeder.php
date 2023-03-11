2340ffe3ca100a40fba9ec8fa3f15fde4fdbc758
muhamad.arsaludin <muhamad.arsaludin@gdn-commerce.com>
2023-03-11 17:12:41 +0700

    refactor: create separated file seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Role;
-use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,21 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::factory()->create(['name' => 'Admin']);
-        Role::factory()->create(['name' => 'Venue']);
-        Role::factory()->create(['name' => 'User']); 
-        User::factory()->create([
-            'role_id' => 1,
-            'name' => 'Admin Onesport',
-            'username' => 'admin',
-            'email' => 'codetarian.inc@gmail.com',
-            'gender' => 'Laki-laki',
-            'city' => 'Tasikmalaya'
-        ]);
-        User::factory(4)->create([
-            'role_id' => 2,
-            'city' => 'Tasikmalaya'
-        ]);
-        User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+        ]);   
     }
 }
