4cc10a5d1995ffe1bba166ea5a78cc4d60297209
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2023-03-12 20:53:55 +0700

    Feature/COTA-33 (#7)
    * feat: create model Venue, Facility & VenueFacility
    
    * feat: create seeder Venue, Facility, VenueFacility
    
    * fix: remove trailing commas and create factory

---
+++ app/Models/VenueFacility.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class VenueFacility extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'venue_id',
+        'facility_id'
+    ];
+
+    public function venue(): BelongsTo
+    {
+        return $this->belongsTo(Venue::class);
+    }
+
+    public function facilities(): HasMany
+    {
+        return $this->hasMany(Facility::class);
+    }
+}
---
+++ database/factories/FacilityFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Facility>
+ */
+class FacilityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => 'facility',
+            'icon' => 'default.svg',
+            'description' => fake()->sentence(),
+            'active' => fake()->numberBetween(0, 1),
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+    }
+}
